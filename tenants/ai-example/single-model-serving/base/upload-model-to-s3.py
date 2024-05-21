ba2c9fee0e5cba635f663250be5de24567e8ff41
Trevor Royer <troyer@redhat.com>
2024-05-21 10:11:13 -0600

    reformat with black

--- tenants/ai-example/single-model-serving/base/upload-model-to-s3.py
+++ tenants/ai-example/single-model-serving/base/upload-model-to-s3.py
@@ -1,26 +1,28 @@
 import boto3, os, botocore
 
 bucket_name = os.getenv("AWS_S3_BUCKET")
-aws_access_key_id = os.environ.get('AWS_ACCESS_KEY_ID')
-aws_secret_access_key = os.environ.get('AWS_SECRET_ACCESS_KEY')
+aws_access_key_id = os.environ.get("AWS_ACCESS_KEY_ID")
+aws_secret_access_key = os.environ.get("AWS_SECRET_ACCESS_KEY")
 endpoint_url = os.getenv("AWS_S3_ENDPOINT")
-region_name = os.environ.get('AWS_DEFAULT_REGION')
+region_name = os.environ.get("AWS_DEFAULT_REGION")
 
-session = boto3.session.Session(aws_access_key_id=aws_access_key_id,
-                    aws_secret_access_key=aws_secret_access_key)
+session = boto3.session.Session(
+    aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key
+)
 
 s3_resource = session.resource(
-    's3',
-    config=botocore.client.Config(signature_version='s3v4'),
+    "s3",
+    config=botocore.client.Config(signature_version="s3v4"),
     endpoint_url=endpoint_url,
-    region_name=region_name)
+    region_name=region_name,
+)
 
 bucket = s3_resource.Bucket(bucket_name)
 
 local_directory = os.getenv("MODEL_PATH")
 s3_prefix = os.getenv("MODEL_PATH")
 
-print('Uploading to s3: ')
+print("Uploading to s3: ")
 for root, dirs, files in os.walk(local_directory):
     for filename in files:
         file_path = os.path.join(root, filename)
@@ -33,7 +35,7 @@ for root, dirs, files in os.walk(local_directory):
 
 prefix = os.getenv("PREFIX_PATH")
 filter = bucket.objects.filter(Prefix=prefix)
-print('Listing Objects: ')
+print("Listing Objects: ")
 for obj in filter.all():
     print(obj.key)
-print('done')
+print("done")
