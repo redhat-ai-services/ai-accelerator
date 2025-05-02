e37c6e6256fdf50ebd2e71c45a94e041db54e6b6
Carl Mes <carl.mes@gmail.com>
2025-05-02 13:22:15 -0600

    Patch

--- scripts/validate_git_config.sh
+++ scripts/validate_git_config.sh
@@ -38,7 +38,7 @@ do
   esac
 done
 
-EXPECTED_REPO=${EXPECTED_REPO:-"https://github.com/redhat-ai-services/ai-accelerator.git"}
+EXPECTED_REPO=${EXPECTED_REPO:-"https://github.com/redhat-mal/ai-accelerator.git"}
 EXPECTED_BRANCH=${EXPECTED_BRANCH:-"main"}
 
 DEBUG=${DEBUG:-false}
